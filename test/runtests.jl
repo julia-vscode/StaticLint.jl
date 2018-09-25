1d2f8c8232f243146c3c07e4c713f609e3f294be
ZacNugent <zac.nugent@gmail.com>
2018-09-25 20:35:25 +0100

    fix test

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,4 +9,4 @@ x = StaticLint.CSTParser.parse(str, true)
 f = StaticLint.File(x)
 StaticLint.pass(f)
 rref, uref = StaticLint.resolve_refs(f.state.refs, f.state)
-@test rref[1].b == f.state.bindings["var1"][1]
+@test rref[1].b == f.state.bindings[()]["var1"][1]
