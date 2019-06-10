4e3052edb09555a88900d3dfb80d23d8375d2608
ZacNugent <zac.nugent@gmail.com>
2019-06-10 23:05:11 +0100

    fix

--- src/type_inf.jl
+++ src/type_inf.jl
@@ -17,7 +17,7 @@ function infer_type(binding::CSTParser.Binding, scope, server)
                         resolve_ref(callname, scope)
                         if hasref(callname)
                             rb = get_root_method(callname.ref, server)
-                            if (rb isa Binding && rb.t == getsymbolserver(server)["Core"].vals["DataType"]) || rb isa SymbolServer.structStore
+                            if (rb isa Binding && (rb.t == getsymbolserver(server)["Core"].vals["DataType"] || rb.val isa SymbolServer.structStore)) || rb isa SymbolServer.structStore
                                 binding.t = rb
                             end
                         end
