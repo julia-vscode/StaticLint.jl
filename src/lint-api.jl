2f370dfa316b3a4ffe6e0b7ce3e57cb402df98a9
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-10-10 19:18:23 -0500

    feat: add lint_string

---
+++ src/lint-api.jl
@@ -0,0 +1,24 @@
+export lint_string
+"""
+    lint_string(str::AbstractString, root = nothing, server = get_symol_server()[1])
+
+Lint the given string and get the result as a CST.
+"""
+function lint_string(str::AbstractString, root = nothing, server = SymbolServer.get_symol_server()[1])
+    # parse
+    cst = CSTParser.parse_code(str)
+
+    # TODO: we should not need to write to a file!
+
+    # Write to file
+    filepath = mktemp()[1]
+    Base.write(filepath, str)
+
+    # File
+    file = StaticLint.File(filepath, str, cst, root, server)
+
+    # Get bindings and scopes
+    StaticLint.scopepass(file)
+
+    return file.cst
+end
