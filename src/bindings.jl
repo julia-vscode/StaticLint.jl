2821b8f13745e22ef6c1940c37a817bb91a782ad
Zac Nugent <zac.nugent@gmail.com>
2021-04-26 22:27:22 +0100

    add tests and missing brackets

--- src/bindings.jl
+++ src/bindings.jl
@@ -327,9 +327,12 @@ function add_binding(x, state, scope=state.scope)
                 if scopehasbinding(tls, name)
                     existing_binding = tls.names[name]
                     if existing_binding isa Binding && (existing_binding.val isa Binding || existing_binding.val isa SymbolServer.FunctionStore || existing_binding.val isa SymbolServer.DataTypeStore)
+                        # Should possibly be a while statement
+                        # If the .val is as above the Binding likely won't have a proper type attached
+                        # so lets use the .val instead.
                         existing_binding = existing_binding.val
                     end
-                    if existing_binding isa Binding && ((existing_binding.type == CoreTypes.Function || existing_binding.type == CoreTypes.DataType) || existing_binding isa SymbolServer.FunctionStore || existing_binding isa SymbolServer.DataTypeStore)
+                    if (existing_binding isa Binding && ((existing_binding.type == CoreTypes.Function || existing_binding.type == CoreTypes.DataType)) || existing_binding isa SymbolServer.FunctionStore || existing_binding isa SymbolServer.DataTypeStore)
                         # do nothing name of `x` will resolve to the root method
                     else
                         seterror!(x, CannotDefineFuncAlreadyHasValue)
