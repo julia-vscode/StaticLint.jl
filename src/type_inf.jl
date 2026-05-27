10ab1c83d14dfeb0042f6b6778f7616c1fc2f9a2
Sebastian Pfitzner <sebastian@juliahub.com>
2026-05-27 22:44:32 +0000

    fix: correctly handle parametrized aliases for method definitions

--- src/type_inf.jl
+++ src/type_inf.jl
@@ -71,6 +71,19 @@ function infer_type_assignment_rhs(binding, state, scope)
                     end
                 end
             end
+        elseif CSTParser.iscurly(rhs)
+            # `const Alias = SomeType{...}` aliases a parameterized type. The
+            # alias is itself a DataType, so adding methods to it is valid.
+            callname = CSTParser.get_name(rhs)
+            if isidentifier(callname)
+                resolve_ref(callname, scope, state)
+                if hasref(callname)
+                    rb = get_root_method(refof(callname), state.server)
+                    if (rb isa Binding && (CoreTypes.isdatatype(rb.type) || rb.val isa SymbolServer.DataTypeStore)) || rb isa SymbolServer.DataTypeStore
+                        settype!(binding, CoreTypes.DataType)
+                    end
+                end
+            end
         elseif headof(rhs) === :INTEGER
             settype!(binding, CoreTypes.Int)
         elseif headof(rhs) === :HEXINT
