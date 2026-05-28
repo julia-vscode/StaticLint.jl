7d98ca0f34e7e139332206628a0e68df56dfc43f
Sebastian Pfitzner <sebastian@juliahub.com>
2026-05-28 00:49:28 +0000

    fix: unwrap wheres for type aliases

--- src/type_inf.jl
+++ src/type_inf.jl
@@ -71,16 +71,21 @@ function infer_type_assignment_rhs(binding, state, scope)
                     end
                 end
             end
-        elseif CSTParser.iscurly(rhs)
-            # `const Alias = SomeType{...}` aliases a parameterized type. The
-            # alias is itself a DataType, so adding methods to it is valid.
-            callname = CSTParser.get_name(rhs)
-            if isidentifier(callname)
-                resolve_ref(callname, scope, state)
-                if hasref(callname)
-                    rb = get_root_method(refof(callname), state.server)
-                    if (rb isa Binding && (CoreTypes.isdatatype(rb.type) || rb.val isa SymbolServer.DataTypeStore)) || rb isa SymbolServer.DataTypeStore
-                        settype!(binding, CoreTypes.DataType)
+        elseif CSTParser.iscurly(rhs) || CSTParser.iswhere(rhs)
+            # `const Alias = SomeType{...}` aliases a parameterized type, possibly
+            # behind `where` clauses (e.g. `const Alias = SomeType{T} where T`).
+            # The alias is itself a type, so adding methods to it is valid. Peel
+            # any `where`/declaration wrappers to get at the underlying `curly`.
+            unwrapped = CSTParser.rem_wheres_decls(rhs)
+            if CSTParser.iscurly(unwrapped)
+                callname = CSTParser.get_name(unwrapped)
+                if isidentifier(callname)
+                    resolve_ref(callname, scope, state)
+                    if hasref(callname)
+                        rb = get_root_method(refof(callname), state.server)
+                        if (rb isa Binding && (CoreTypes.isdatatype(rb.type) || rb.val isa SymbolServer.DataTypeStore)) || rb isa SymbolServer.DataTypeStore
+                            settype!(binding, CoreTypes.DataType)
+                        end
                     end
                 end
             end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3053,4 +3053,36 @@ end
         )
         @test !has_error(cst, StaticLint.CannotDefineFuncAlreadyHasValue)
     end
+    # Aliasing a `UnionAll` via a `where` clause is also a valid constructor target.
+    let cst = parse_and_pass(
+            """
+            const MyVec = Vector{T} where T
+
+            MyVec(x::Int64) = [x]
+            """
+        )
+        @test !has_error(cst, StaticLint.CannotDefineFuncAlreadyHasValue)
+    end
+    # Multiple type variables in the `where` clause.
+    let cst = parse_and_pass(
+            """
+            const MyArray = Array{T,N} where {T,N}
+
+            MyArray(x::Int64) = [x]
+            """
+        )
+        @test !has_error(cst, StaticLint.CannotDefineFuncAlreadyHasValue)
+    end
+    # User-defined struct aliased through a `where` clause.
+    let cst = parse_and_pass(
+            """
+            module M
+            struct Foo{T} end
+            const Bar = Foo{T} where T
+            Bar(x::Int64) = 1
+            end
+            """
+        )
+        @test !has_error(cst, StaticLint.CannotDefineFuncAlreadyHasValue)
+    end
 end
